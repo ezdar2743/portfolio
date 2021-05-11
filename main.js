e7577a937f96f92eab4be62c01157073e1469948
ezdar2743 <ezdar@naver.com>
2021-05-11 14:41:52 +0900

    js_navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict'
+//Navbar scroll 
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() =>{
+    
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add("navbar__dark")
+    }else{
+        navbar.classList.remove("navbar__dark")
+    }
+})
